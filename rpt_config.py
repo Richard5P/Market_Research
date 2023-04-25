da55cafba3f5b683a6cad67b023bdb35a41ecc93
Richard5P <richard.deutsch@gmail.com>
2023-04-25 09:25:17 +0000

    fix: Update rpt_config to correct remaining pylint errors and warnings

--- rpt_config.py
+++ rpt_config.py
@@ -2,7 +2,7 @@ import datetime
 
 """
 This file contains the classes, function and variables for
-the user processes for configuring the report calculations. 
+the user processes for configuring the report calculations.
 The file is meant to be imported into and called from a parent file
 for deployment.
 """
@@ -41,7 +41,7 @@ def years_loaded(stat_dict):
                 for stat in country['statistics']:
                     years_loaded.add(stat['year'])
         else:
-            continue             
+            continue
     first_year = min(years_loaded)
     last_year = max(years_loaded)
     print(first_year, last_year)
@@ -63,11 +63,11 @@ def input_years(stat_dict):
         try:
             start_year = input('Report start year:\n')
             end_year = input('Report end year:\n')
-            if not (start_year >= range_of_years[0] and 
-            end_year <= range_of_years[1]):
+            if not (start_year >= range_of_years[0] and
+               end_year <= range_of_years[1]):
                 raise InvalidDateRange
             else:
-                return([start_year, end_year]) 
+                return ([start_year, end_year])
         except InvalidDateRange:
             print(f'\nYears must be between {range_of_years[0]} and )'
                   f'{range_of_years[1]}, please try again')
@@ -82,9 +82,9 @@ def regions_loaded(stat_dict):
     for code in stat_dict:
         if code['stats_code'] == 'popu':
             for country in code['country_stats']:
-                regions_loaded.add(stat['region_code'])
+                regions_loaded.add(country['region_code'])
         else:
-            continue           
+            continue
     return (sorted(regions_loaded))
 
 
@@ -97,15 +97,15 @@ def input_regions(stat_dict):
     print(f'The following is a list of available regions.')
     print(f'Please use the exactly the same region code as in the list.')
     print(f'If you would like to report on more than one region, then'
-        f'please separate them with commas similarly to the list.')
+          f'please separate them with commas similarly to the list.')
     regions_unset = True
     while regions_unset:
         try:
             report_regions = input('Report region(s):\n')
-            if(all(regions in report_regions for regions in region_list)):
+            if (all(regions in report_regions for regions in region_list)):
                 raise InvalidRegion
             else:
-                return (report_regions)  
+                return (report_regions)
         except InvalidRegion:
             print(f'\nRegions must be from the list and in the same format, '
                   f'please try again')
@@ -115,9 +115,10 @@ def input_weights():
     """
     Prompts user for study weight report configuration
     """
-    print(f'There are 3 report studies available for your report:'\
-        f'\t Disposable Income, Population, Urbanisation\n')
-    print(f'Please enter 3 numbers which total to 100 for weighting the percent of each study')
+    print(f'There are 3 report studies available for your report:'
+          f'\t Disposable Income, Population, Urbanisation\n')
+    print(f'Please enter 3 numbers which total to 100 for weighting the'
+          f' percent of each study')
     pct_unset = True
     while pct_unset:
         try:
@@ -127,9 +128,9 @@ def input_weights():
             if ((disp_pct + popu_pct + urba_pct) != 100):
                 raise InvalidPercents
             else:
-                return([disp_pct,popu_pct,urba_pct])
+                return ([disp_pct, popu_pct, urba_pct])
         except InvalidPercents:
-            print('\nAmounts entered do not sum to 100, please try again') 
+            print('\nAmounts entered do not sum to 100, please try again')
         except ValueError:
             print('\nNumbers only, please try again')
 
@@ -143,4 +144,4 @@ def input_rpt_options(weights, years, regions, stat_dict):
     weights = input_weights()
     years = input_years(stat_dict)
 #    regions = input_regions()
-    return([weights, years])
+    return ([weights, years])
