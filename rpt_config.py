ff80c7f21564ddc504713b33e0aaf230008f5dda
Richard5P <richard.deutsch@gmail.com>
2023-04-25 08:47:14 +0000

    feat: Add input_regions to rpt_config

--- rpt_config.py
+++ rpt_config.py
@@ -1,11 +1,12 @@
 import datetime
 
-
 """
 This file contains the classes, function and variables for
 the user processes for configuring the report calculations. 
 The file is meant to be imported into and called from a parent file for deployment.
 """
+
+
 class InvalidPercents(Exception):
     """
     Raise when percents don't add up to 100
@@ -14,6 +15,13 @@ class InvalidPercents(Exception):
 
 
 class InvalidDateRange(Exception):
+    """
+    Raise when years are out of range
+    """
+    pass
+
+
+class InvalidRegion(Exception):
     """
     Raise when years are out of range
     """
@@ -36,7 +44,7 @@ def years_loaded(stat_dict):
     first_year = min(years_loaded)
     last_year = max(years_loaded)
     print(first_year, last_year)
-    return([first_year, last_year])
+    return ([first_year, last_year])
 
 
 def input_years(stat_dict):
@@ -46,8 +54,9 @@ def input_years(stat_dict):
     range_of_years = years_loaded(stat_dict)
     print(f'\nNow choose the range of years:')
     print(f'The range of years studies available for your report are '
-        f'from: {range_of_years[0]} to: {range_of_years[1]}')
-    print(f'\nPlease enter a start year and an end year within that range (inclusive)')
+          f'from: {range_of_years[0]} to: {range_of_years[1]}')
+    print(f'\nPlease enter a start year and an end year within that '
+          f'range (inclusive)')
     years_unset = True
     while years_unset:
         try:
@@ -60,29 +69,30 @@ def input_years(stat_dict):
                 return([start_year, end_year]) 
         except InvalidDateRange:
             print(f'\nYears must be between {range_of_years[0]} and )'
-                f'{range_of_years[1]}, please try again')
+                  f'{range_of_years[1]}, please try again')
 
 
 def input_regions(stat_dict):
     """
     Prompts user to select region(s) for report configuration
     """
-    regions_loaded(stat_dict)
-    print(f'\nNow choose the range of years:')
-    print(f'The range of years studies available for your report are '
-        f'from: {range_of_years[0]} to: {range_of_years[1]}')
-    print(f'\nPlease enter a start year and an end year within that range (inclusive)')
-    years_unset = True
-    while years_unset:
+    region_list = (regions_loaded(stat_dict))
+    print(f'\nNow choose the region or regions to report on.')
+    print(f'The following is a list of available regions.')
+    print(f'Please use the exactly the same region code as in the list.')
+    print(f'If you would like to report on more than one region, then'
+        f'please separate them with commas similarly to the list.')
+    regions_unset = True
+    while regions_unset:
         try:
-            start_year = input('Report start year:\n')
-            end_year = input('Report end year:\n')
-            if not (start_year >= range_of_years[0] and end_year <= range_of_years[1]):
-                raise InvalidDateRange
+            report_regions = input('Report region(s):\n')
+            if(all(regions in report_regions for regions in region_list)):
+                raise InvalidRegion
             else:
-                return([start_year, end_year])     
-        except InvalidDateRange:
-            print(f'\nYears must be between {range_of_years[0]} and {range_of_years[1]}, please try again')
+                return (report_regions)     
+        except InvalidRegion:
+            print(f'\nRegions must be from the list and in the same format, '
+                f'please try again')
 
 
 def input_weights():
