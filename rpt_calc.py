18fe9b583fbd55ac3ad05d8b01800e4525339e01
Richard5P <richard.deutsch@gmail.com>
2023-05-04 10:01:56 +0000

    feat: Add calc_region and replace sum_report_data with avg_report_data to calculate regional reporting values

--- rpt_calc.py
+++ rpt_calc.py
@@ -6,24 +6,48 @@ deployment.
 """
 
 
+def calc_region(sum_region, region_data, stats_dict):
+    """
+    Checks if region has sum_value and returns it for
+    use in summing region values for report
+    """
+    for i in range(0, len(region_data)):
+        if sum_region in region_data[i].values():
+            sum_value = region_data[i]['sum_value']
+        else:
+            sum_value = 0
+    return (sum_value)
+
+
 def avg_report_data(years, regions, stats_dict):
     """
     Using the report options, calculate the average statistic values
     for each region and type of statistic across the range of years.
-    
+    The calculations will support regional reporting and future
+    country reporting.
+        country_avg =
+            sum_value (for range of years)/number of years
+        region_avg =
+            sum country_annual_avg (for countries in regaion)/
+                number of countries in region
     Return a list of lists: [region code, stat code, avg value]
+    Assumes the stats_dict is sorted by country by region
     """
-    sum_data = []
-    sum_value = 0
+    num_years = int(years[1]) - int(years[0])
+    region_data = [{'region': None, 'sum_value': None, 'num_countries': 0}]
+    region_value = 0
     for country in stats_dict:
         if country['region_code'] in regions:
             sum_region = country['region_code']
+            region_value = calc_region(sum_region, region_data, stats_dict)
             for statistic in country['statistic']:
                 sum_stat_code = statistic['stats_code']
                 if statistic['year'] >= years[0] and \
                    statistic['year'] <= years[1]:
                     sum_value += float(statistic['value'])
-            sum_data.append([sum_region, sum_stat_code, sum_value])
+                country_avg = sum_value / num_years
+            avg_value = sum_value / num_countries
+            sum_data.append([sum_region, sum_stat_code, avg_value])
     return (sum_data)
 
 
